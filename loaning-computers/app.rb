0bf1f7f14c489ce0e8ddbd719de21d8d86262b7a
itggot-elias-carlson <elias.carlson@itggot.se>
2017-10-12 09:52:05 +0200

    La till routes

--- loaning-computers/app.rb
+++ loaning-computers/app.rb
@@ -5,3 +5,34 @@ get('/') do
     erb(:index)
 end
 
+get('/students/:id') do
+    db = SQLite3::Database.new("computers_and_loans.sqlite")
+    student_id = params[:id]
+    result = db.execute("SELECT * FROM students WHERE id="+student_id)
+    result = result[0]
+    erb(:students, locals:{ student:result} )
+end
+
+get('/students/:id/update') do
+
+end
+
+get('/students/:id/delete') do
+
+end
+
+get('/students/create') do
+
+end
+
+post('/students/:id/update') do
+
+end
+
+post('/students/:id/delete') do
+
+end
+
+post('/students/create') do
+
+end
